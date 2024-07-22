09136e219b8b5ada73f57ce3a0bc4b98403c9c00
Shaylin Chetty <shaylin.chetty@protonmail.com>
2024-07-22 19:18:14 +0200

    chore: fix z-index issue

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,7 +4,7 @@ import ResponsiveNavMenu from "@/components/responsiveNavMenu/responsiveNavMenu"
 export default function Header() {
   return (
     <header
-      className="fixed flex flex-col justify-center items-center w-full h-24 top-0 bg-dark-tone drop-shadow-md">
+      className="fixed flex flex-col justify-center items-center w-full h-24 top-0 bg-dark-tone drop-shadow-md z-50">
       <div className="flex flex-row justify-between items-center grow max-w-screen-lg my-2 mx-4 gap-12">
         <BrandLogo/>
         <ResponsiveNavMenu/>
