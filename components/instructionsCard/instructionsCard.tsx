cb9069325c2ce2ad7c0e5a8034f523953bfb518f
Shaylin Chetty <shaylin.chetty@protonmail.com>
2024-09-26 21:50:08 +0200

    chore: add redirection link

--- components/instructionsCard/instructionsCard.tsx
+++ components/instructionsCard/instructionsCard.tsx
@@ -1,12 +1,13 @@
 import React, { JSX } from "react";
 import { Divider } from "@nextui-org/divider";
+import Link from "next/link";
 
 export default function InstructionsCard(): JSX.Element {
   return (
     <div className="text-center flex flex-col gap-1">
-      <p className="text-xl font-bold">This site has been deprecated. </p>
+      <p className="text-xl font-bold">This site has been deprecated. Instead visit: </p>
       <Divider/>
-      <p className="text-lg font-bold">Instead visit: https://huggingface.co/spaces/Ashikan/dut-recipe-generator</p>
+      <Link className="text-lg font-bold text-highlight-tone" href="https://huggingface.co/spaces/Ashikan/dut-recipe-generator">https://huggingface.co/spaces/Ashikan/dut-recipe-generator</Link>
     </div>
   );
 }
