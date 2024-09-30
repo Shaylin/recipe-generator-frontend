d938a5807e8653da81b252f264d820315d8bebcb
Shaylin Chetty <shaylin.chetty@protonmail.com>
2024-09-30 18:36:34 +0200

    chore: add server side redirect

--- components/recipeGenerator/recipeGenerator.tsx
+++ components/recipeGenerator/recipeGenerator.tsx
@@ -1,13 +1,6 @@
-"use client"
-
-import React, { JSX } from "react";
-import InstructionsCard from "@/components/instructionsCard/instructionsCard";
+import { JSX } from "react";
+import { redirect } from "next/navigation";
 
 export default function RecipeGenerator(): JSX.Element {
-  return (
-    <div
-      className="flex flex-col items-center justify-around max-w-screen-lg min-h-96 sm:w-10/12 w-full bg-light-tone p-6 rounded-xl shadow gap-8">
-      <InstructionsCard/>
-    </div>
-  )
+  redirect("https://huggingface.co/spaces/Ashikan/dut-recipe-generator");
 }
